237911955776fb996a94e4afccd52b65fba14520
Daniel-GM <danielgomesmoura@gmail.com>
2023-04-06 09:48:08 -0300

    primeiro commit

---
+++ js/web_scraping.js
@@ -0,0 +1,11 @@
+fetch('https://gol.gg/players/player-stats/1114/season-S13/split-Spring/tournament-ALL/champion-ALL/')
+  .then(response => response.text())
+  .then(html => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, 'text/html');
+    const title = doc.querySelector('title').textContent;
+    console.log(title);
+  })
+  .catch(error => {
+    console.log(error);
+  });
